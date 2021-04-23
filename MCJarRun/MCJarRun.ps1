f4d3adc9cfb4368fa2e3c309472c696b29531775
Vukky <46850780+Vukky123@users.noreply.github.com>
2021-04-23 09:27:36 +0200

    Update MCJarRun.ps1

--- MCJarRun/MCJarRun.ps1
+++ MCJarRun/MCJarRun.ps1
@@ -1,4 +1,18 @@
-$mclocation = Read-Host -Prompt "Enter the full path to your Minecraft runtime folder"
-$jarlocation = Read-Host -Prompt "Enter the full path to the JAR you want to execute"
+param (
+    [string]$mcpath = '',
+    [string]$jarpath = '',
+    [string]$noconsole = $false
+)
 
-Start-Process $mclocation/jre-x64/bin/java.exe -ArgumentList '-jar', "$jarlocation"
+if($mcpath -eq '') {
+    $mcpath = Read-Host -Prompt "Enter the full path to your Minecraft runtime folder"
+}
+if($jarpath -eq '') {
+    $jarpath = Read-Host -Prompt "Enter the full path to the JAR you want to execute"
+}
+
+if($noconsole -eq $true) {
+    Start-Process $mcpath/jre-x64/bin/javaw.exe -ArgumentList '-jar', "$jarpath"
+} else {
+    Start-Process $mcpath/jre-x64/bin/java.exe -ArgumentList '-jar', "$jarpath"
+}
