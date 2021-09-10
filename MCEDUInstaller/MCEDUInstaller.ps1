2d57d0e0a2c4517162e3015c2f86d499a756734e
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-09-10 10:54:16 +0200

    Update MCEDUInstaller.ps1

--- MCEDUInstaller/MCEDUInstaller.ps1
+++ MCEDUInstaller/MCEDUInstaller.ps1
@@ -14,9 +14,8 @@ Function pause ($message)
 }
 
 Clear-Host
-Write-Host "Minecraft: Education Edition - PowerShell Installer"
-Write-Host "Vukky Limited is not responsible for any damages to your computer caused by this installer."
-$InstallType = Read-Host -Prompt "Install, Update, or Uninstall"
+Write-Host "Welcome to the Minecraft: Education Edition installer!"
+$InstallType = Read-Host -Prompt "Do you want to install, update, or uninstall the game?"
 if ($InstallType -ne "Install" -And $InstallType -ne "Update" -And $InstallType -ne "Uninstall") {
     Write-Host "$InstallType is not Install, Update, or Uninstall."
     $InstallType = "Install"
@@ -24,9 +23,6 @@ if ($InstallType -ne "Install" -And $InstallType -ne "Update" -And $InstallType 
 }
 try { 
   Clear-Host
-  if ($InstallType -eq "Install" -Or $InstallType -eq "Update") {
-    pause "Alert! If you will be playing with other devices, make sure they're on the latest MC:EDU version, or you won't be able to connect to them. `nPress any key to continue."
-  }
   Clear-Host
   if ($InstallType -eq "Install") {
     Write-Host "Installing Minecraft: Education Edition... `nThis might take a while. Please be patient."
@@ -39,11 +35,7 @@ try {
     Add-AppxPackage -Update https://aka.ms/downloadmee
   }
   if ($InstallType -eq "Uninstall") {
-    pause "Are you SURE you want to uninstall Minecraft: Education Edition? All Minecraft: Education Edition data on this device will be LOST. `nIf yes, press any key.`nIf no, please close this window."
-    Clear-Host
-    pause "THIS INSTALLER WILL NOT RESTORE YOUR DATA. ALL MINECRAFT: EDUCATION EDITION DATA on this device WILL BE LOST. ARE YOU STILL SURE? `nIf yes, press any key.`nIf no, please close this window."
-    Clear-Host
-    pause "THIS IS YOUR LAST CHANCE. DO YOU WANT TO UNINSTALL AND LOSE ALL MINECRAFT: EDUCATION EDITION DATA ON THIS DEVICE FOREVER? `nIf yes, press any key.`nIf no, please close this window."
+    pause "Are you SURE you want to uninstall Minecraft: Education Edition? You'll lose all your worlds and stuff!`nPress any key to continue, or close this window."
     Clear-Host
     Write-Host "Uninstalling Minecraft: Education Edition... `nAll your Minecraft: Education Edition data on this device is being removed."
     $MCEE = Get-AppxPackage Microsoft.MinecraftEducationEdition
@@ -52,7 +44,7 @@ try {
   Clear-Host
   if ($InstallType -eq "Install" -Or $InstallType -eq "Update") {
       $MCEE = Get-AppxPackage Microsoft.MinecraftEducationEdition
-      Write-Host "Minecraft: Education Edition" $MCEE.Version "installed!"
+      Write-Host "Minecraft: Education Edition" $MCEE.Version " has been installed!"
   }
   if ($InstallType -eq "Uninstall") {
     Write-Host "Minecraft: Education Edition" $MCEE.Version "has been uninstalled. `nYour Minecraft: Education Edition data has been removed from this device."
@@ -61,7 +53,7 @@ try {
 }
 catch {
   Clear-Host
-  Write-Host "Uh oh, something went wrong."
+  Write-Host "Uh oh, something went wrong. Please send this error to Vukky!"
   Write-Host $_
   pause "Press any key to exit."
 }
