c7dfc5179c355de045c6a096bb0acbbb18cc3670
Vukky <46850780+Vukkyy@users.noreply.github.com>
2021-11-08 11:20:29 +0100

    Update MCEDUInstaller.ps1

--- MCEDUInstaller/MCEDUInstaller.ps1
+++ MCEDUInstaller/MCEDUInstaller.ps1
@@ -1,3 +1,6 @@
+param (
+    [string]$InstallType = '',
+)
 Function pause ($message)
 {
     # Check if running Powershell ISE
@@ -16,7 +19,9 @@ Function pause ($message)
 Clear-Host
 Write-Host "Welcome to the unofficial Minecraft: Education Edition installer!"
 Write-Host "(c) LIT Devs 2021`n`n"
-$InstallType = Read-Host -Prompt "Do you want to install, update, or uninstall the game?`nType Install, Update, or Uninstall"
+if ($InstallType -eq "") {
+    $InstallType = Read-Host -Prompt "Do you want to install, update, or uninstall the game?`nType Install, Update, or Uninstall"
+}
 if ($InstallType -ne "Install" -And $InstallType -ne "Update" -And $InstallType -ne "Uninstall") {
     Write-Host "$InstallType is not Install, Update, or Uninstall."
     $InstallType = "Install"
