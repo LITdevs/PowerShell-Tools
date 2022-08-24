759d76dbc31c144db2622afa2e4644a3d02d2362
Vukky <46850780+Vukkyy@users.noreply.github.com>
2022-08-24 13:17:54 +0200

    i genuinely pray to god this is how it works

--- MCEDUInstaller/MCEDUInstaller.ps1
+++ MCEDUInstaller/MCEDUInstaller.ps1
@@ -41,8 +41,10 @@ try {
     Add-AppxPackage -Update https://aka.ms/downloadmee
   }
   if ($InstallType -eq "Uninstall") {
-    pause "Are you SURE you want to uninstall Minecraft: Education Edition?`nYou'll lose all your data in the game!`nPress any key to continue, or close this window."
-    Clear-Host
+    if(-Not $BypassUninstallConfirm) {
+      pause "Are you SURE you want to uninstall Minecraft: Education Edition?`nYou'll lose all your data in the game!`nPress any key to continue, or close this window."
+      Clear-Host
+    }
     Write-Host "Uninstalling Minecraft: Education Edition... `nAll your Minecraft: Education Edition data on this device is being removed."
     $MCEE = Get-AppxPackage Microsoft.MinecraftEducationEdition
     Remove-AppxPackage -Package $MCEE.PackageFullName
